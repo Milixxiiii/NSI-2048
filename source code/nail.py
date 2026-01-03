bc688871a49afd65a129325fcb6c31150e7ef50a
nailprime <nailprimebiz@gmail.com>
2026-01-03 15:43:37 +0100

    test tkinter

--- source code/nail.py
+++ source code/nail.py
@@ -6,3 +6,19 @@ def fleche_gauche():
         for x in ligne:
             if x != 0:
                 nvl_ligne.append(x)
+
+# TEST TKINTER 1:
+import tkinter as tk
+from tkinter import messagebox
+
+fenetre = tk.Tk()
+fenetre.title("Test Tkinter")
+fenetre.geometry("300x150")  
+
+def bouton_clique():
+    messagebox.showinfo("clique")
+
+bouton = tk.Button(fenetre, text="Clique moi", command=bouton_clique)
+bouton.pack(pady=50)  # pady = marge verticale
+
+fenetre.mainloop()

bea606d6e5a370fca585444a1a26c33072fccf09
PaRaNoRiP-999 <merwanrazzak12@gmail.com>
2026-01-04 14:14:42 +0100

    merwan.py

--- source code/merwan.py
+++ source code/merwan.py
@@ -1,9 +1,22 @@
 def menu():
     print("1 : Lancer la partie")
     print("2 : Changer le thème")
-    print("3 : ableau des scores")
+    print("3 : Tableau des scores")
     print("4 : Crédits")
-    a=int(input("Faites votre choix entre 1 et 4 : "))
-    print("Milo le goat",a)
-
-menu()
+    choix = input("Faites votre choix entre 1 et 4 : ")
+    if choix == "1":
+        print("La partie commence...")
+        # Lancer la partie
+    elif choix == "2":
+        print ("Changement du thème...")
+        # Changer le thème
+    elif choix == "3":
+        print("Affichage des scores...")
+        # Afficher les scores
+    elif choix == "4":
+        print ("Crédits")
+        # Afiicher les crédits
+    else:
+        print("Choix invalide !")
+        menu()
+        # Relance le menu
